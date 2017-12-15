bd62b02b212a5276b5eff8a8fa3938923a388209
YomiGUO <jesuiguo@gmail.com>
2017-12-15 10:29:02 +0100

    convolution with right kernel order

---
+++ model/generic/C_C++_code/RELU/relu_simple_reference.cpp
@@ -0,0 +1,7 @@
+#include "relu_simple_reference.h"
+
+RELU_SIMPLE_REFERENCE::RELU_SIMPLE_REFERENCE()
+{
+
+}
+
---
+++ model/generic/C_C++_code/convolution.h
@@ -0,0 +1,11 @@
+#ifndef CONVOLUTION_H
+#define CONVOLUTION_H
+
+
+class convolution
+{
+public:
+    convolution();
+};
+
+#endif // CONVOLUTION_H
