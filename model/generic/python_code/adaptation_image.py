bc6d1f5a16e4d527efb4be5b5829f392fb782d27
YomiGUO <jesuiguo@gmail.com>
2017-12-15 23:23:51 +0100

    normalzation -1 and 1

--- model/generic/python_code/adaptation_image.py
+++ model/generic/python_code/adaptation_image.py
@@ -8,31 +8,49 @@ def adapatation_imag(image):
 	imag_y_max=image.shape[0]
 	imag_x_max=image.shape[1]
 	imag_NB_pixel=image.shape[2]
-	u=np.zeros((imag_NB_pixel))	
-	sigma=np.zeros((imag_NB_pixel))
-	carre=np.zeros((imag_x_max*imag_y_max))
+	#u=np.zeros((imag_NB_pixel))	
+	#sigma=np.zeros((imag_NB_pixel))
+	carre=np.zeros((imag_x_max*imag_y_max,imag_NB_pixel))
 	new_image=np.zeros((imag_y_max,imag_x_max,imag_NB_pixel))
 	new_image_out=np.zeros((24,24,imag_NB_pixel))
+	u=(image[:,:,:].sum())/(imag_x_max*imag_y_max*imag_NB_pixel)
 	for num in range (0,imag_NB_pixel):
-		u[num]=(image[:,:,num].sum())/(imag_x_max*imag_y_max)
+		
 
 		for num2 in range(0,imag_y_max):
 			for num3 in range(0,imag_x_max):
-				carre[num2*imag_x_max+num3]=math.pow(image[num2,num3,num]-u[num],2)
-		sigma[num]=math.sqrt((carre.sum())/(imag_x_max*imag_y_max))
-		
+				carre[num2*imag_x_max+num3,num]=math.pow(image[num2,num3,num]-u,2)
+	sigma=math.sqrt((carre.sum())/(imag_x_max*imag_y_max))
+	
+	for num in range (0,imag_NB_pixel):	
 		for y in range(0,imag_y_max):
 			for x in range(0,imag_x_max):
-				if sigma[num] > (1/math.sqrt(imag_x_max*imag_y_max)):
-					new_image[y,x,num]=((image[y,x,num]-u[num])/sigma[num])
+				if sigma > (1/math.sqrt(imag_x_max*imag_y_max)):
+					if((image[y,x,num]-u)/sigma)>1:
+						new_image[y,x,num]=1
+					elif((image[y,x,num]-u)/sigma)<-1:
+						new_image[y,x,num]=-1
+					else:
+						new_image[y,x,num]=((image[y,x,num]-u)/sigma)
+				
 				else:
-						new_image[y,x,num]=(image[y,x,num]-u[num])/(1/math.sqrt(imag_x_max*imag_y_max))
+					if(image[y,x,num]-u)/(1/math.sqrt(imag_x_max*imag_y_max))>1:
+						new_image[y,x,num]=1
+					elif(image[y,x,num]-u)/(1/math.sqrt(imag_x_max*imag_y_max))<-1:
+						new_image[y,x,num]=-1
+					else:
+						new_image[y,x,num]=(image[y,x,num]-u)/(1/math.sqrt(imag_x_max*imag_y_max))
 				
 		
 
 	n=int((imag_y_max-24)/2)
 	new_image_out=new_image[n-1:24+(n-1),n-1:24+(n-1),:]
 	return new_image_out
+
+
+
+
+
 
 
 
